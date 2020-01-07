9e1d4593c2263de586bee156db3cd35fea558768
David Balan <sc@silvercrow.org>
2020-01-07 01:38:46 -0600

    Update app.js
    updating song name

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function Write(word){
 g.clear();
   g.setFontDennis8();
   g.drawString("\x90 \x91\x92\x93\x94\x95\x96\x97 "+word+" \x9f \x8c \x8d \x89",0,0);
-g.drawString("\xa0 Bäh böse Ärzte in Küche\xaf",0,24);
+g.drawString("\xa0 You Only Live Twice\xaf",0,24);
   //g.drawString(word,30,10);
   g.flip();
 }
